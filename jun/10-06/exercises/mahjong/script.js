600a5493541c77322af47f44e50667f2bb41a4ef
aliceheneghan <heneghan31@gmail.com>
2022-06-10 16:46:39 +0200

    added notes to help understand all the functions and their interactions

--- jun/10-06/exercises/mahjong/script.js
+++ jun/10-06/exercises/mahjong/script.js
@@ -1,171 +1,135 @@
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
 let currentValue = 0;
-let tileMarked = -1;
+let tileMarked = -1; // variable ready to be used in case a tile has been clicked
 
-let mistakes = 3;
+let mistakes = 3; // countdown will begin at 3
 
 function initialize() {
-
-    setDefaultValues(true);
-
-    // remove all tiles before creating new ones
-    const tiles = document.querySelectorAll('.tile')
-
-    tiles.forEach(tile => tile.remove())
-
-    for (let i = 0; i < 10; i++) {
-
-        const tile = document.createElement('div')
-        tile.classList.add('tile')
-        tile.textContent = randomNumber()
-        tile.id = i;
-        body.appendChild(tile);
-        tile.onclick = handleTileCLick
-    }
-
-
+  setDefaultValues(true);
+  // remove all tiles before creating new ones?
+  const tiles = document.querySelectorAll(".tile"); // stores all tiles with this class
+  tiles.forEach((tile) => tile.remove());
+  for (let i = 0; i < 10; i++) {
+    const tile = document.createElement("div");
+    tile.classList.add("tile"); // this adds the class tile to the div
+    tile.textContent = randomNumber(); // uses random number function to create random number for tile
+    tile.id = i; // gives each tile an id using the iteration number
+    body.appendChild(tile); // adds tile div to HTML document
+    tile.onclick = handleTileCLick; // calls handleTileClick function when tile is clicked
+  }
 }
-initialize()
-
+initialize();
 function randomNumber() {
-    return Math.ceil(Math.random() * 10)
+  return Math.ceil(Math.random() * 10); // Math.ceil instead of Math.floor to avoid 0
 }
 
 // cases:
 // 1. user clicked a tile that is not marked
-// 2. user clicked a tile that is marked    
+// 2. user clicked a tile that is marked
 // 3. user clicked the same tile twice
 // 4. user clicked a different tile
 // 5. user clicked a tile that is not correct
 // 6. user clicked the correct tile
 
-function handleTileCLick(event) {
-
-    const tile = document.getElementById(event.target.id);
-
-    if (currentValue > 0) { // there is a tile clicked
-
-        if (tileMarked == event.target.id) { // user clicked the same tile twice
-
-            tile.classList.remove('marked')
-            setDefaultValues()
-        } else { // user clicked a different tile
-        
-            if (tile.textContent == currentValue) { // user clicked the correct tile
-                removeTiles(tile.id, tileMarked)
-                setDefaultValues()
-            } else { // user clicked the wrong tile
-                mistakes--;
-                updateMistakes();
-                if (mistakes == 0) {
-                    lost(false)
-                }
-            }
+function handleTileCLick(event) { // event comes from tile.onclick line 19
+  const tile = document.getElementByID(event.target.id); // target is the element where the event has taken place, whole line stores target id in tile variable
+  if (currentValue > 0) { // currentValue set to 0 in line 3, if it's more, there is a tile clicked
+    if (tileMarked == event.target.id) { // event.target.id set to -1 on first click in line 
+      tile.classList.remove("marked"); // marked class added on line 57, deselects tile
+      setDefaultValues(); // resets tile values to default
+    } else { // if the user clicked a different tile
+        if (tile.textContent == currentValue) { // user clicked a matching tile
+        removeTiles(tile.id, tileMarked);
+        setDefaultValues(); // resets all other tiles?
+      } else { // user clicked the wrong tile
+        mistakes--; // deducts 1 from mistakes variable line 6
+        updateMistakes();
+        if (mistakes == 0) {
+          lost(false);
         }
-        
-
-    } else { // first time a tile is clicked
-        console.log('tilemarked', tileMarked, 'tileid', tile.id)
-       
-        currentValue = event.target.textContent;
-        tileMarked = event.target.id;
-
-        // change background color
-        
-        tile.classList.add('marked')
-        
+      }
     }
-
-    if (!checkForMoreMoves()) setTimeout(() => lost(true), 100) // need to add some time to wait for the DOM to update
-
-    console.log('clicked: event=', event.target.textContent)
-    // currentValue = event.targe
+  } else { // first time a tile is clicked
+    console.log("tilemarked", tileMarked, "tileid", tile.id);
+    currentValue = event.target.textContent;
+    tileMarked = event.target.id;
+    tile.classList.add("marked"); // changes background color by adding new css class, line 31
+  }
+  if (!checkForMoreMoves()) setTimeout(() => lost(true), 100); // need to add some time to wait for the DOM to update
+  console.log("clicked: event=", event.target.textContent);
+  currentValue = event.target
 }
-
 function removeTiles(a, b) {
-    const tileA = document.getElementById(a);
-    
-    const tileB = document.getElementById(b);
-
-    tileA.remove();
-    tileB.remove();
+  const tileA = document.getElementById(a); // tile.id stored in 'a' on line 43
+  const tileB = document.getElementById(b); // tileMarked value stored in b on line 43
+  // if they match (questioned on line 42) remove them both
+  tileA.remove();
+  tileB.remove();
 }
 
-function setDefaultValues(newGame){
-
-    currentValue = 0;
-    tileMarked = -1;
-    if (newGame) mistakes = 3;
+function setDefaultValues(newGame) {
+  currentValue = 0;
+  tileMarked = -1;
+  if (newGame) mistakes = 3;
 }
 
 function checkPlayerWon() {
-    const tiles = document.querySelectorAll('.tile')
-    if (tiles.length == 0) {
-        alert('you won!')
-    }
+  const tiles = document.querySelectorAll(".tile");
+  if (tiles.length == 0) {
+    alert("you won!");
+  }
 }
 
 function checkForMoreMoves() {
-    
-    console.log('checkForMoreMoves here')
-    const tiles = [...document.querySelectorAll('.tile')]
-    
-    if (tiles.length < 2) return false;
-    
-    const cleanedTiles = tiles.reduce((arr, tile) => {
+  console.log("checkForMoreMoves here");
+  const tiles = [...document.querySelectorAll(".tile")];
 
-        console.log('in reduce', tile.textContent)
-        console.log('in reduce', arr)
+  if (tiles.length < 2) return false;
 
-        if(!arr.includes(tile.textContent)){
-            arr.push(tile.textContent)
-            
-        }
-        return arr
+  const cleanedTiles = tiles.reduce((arr, tile) => {
+    console.log("in reduce", tile.textContent);
+    console.log("in reduce", arr);
+
+    if (!arr.includes(tile.textContent)) {
+      arr.push(tile.textContent);
     }
-    , [])
-    console.log('tiles: cleaned/original', cleanedTiles.length, tiles.length)
+    return arr;
+  }, []);
+  console.log("tiles: cleaned/original", cleanedTiles.length, tiles.length);
 
-    if (cleanedTiles.length < tiles.length) return true;
+  if (cleanedTiles.length < tiles.length) return true;
 
-    return false;
+  return false;
 }
 
 function lost(moves) {
-
-    if (moves) {
-        alert('Oh no! no more moves! you can play again')
-
-    } else {
-
-        alert('Oh no! Too many mistakes! you can play again')
-    }
-
-    setDefaultValues(true)
-    updateMistakes();
-    
-    const footer = document.createElement('div')
-    footer.classList.add('footer')
-    const button = document.createElement('button')
-    button.textContent = 'Play again'
-    button.onclick = handlePlayAgain
-    footer.appendChild(button)
-    body.appendChild(footer)
-
-    // make rest tiles unclickable
-    const tiles = document.querySelectorAll('.tile')
-    tiles.forEach(tile => tile.onclick = null)
+  if (moves) { // if moves true
+    alert("Oh no! no more moves! you can play again"); // alert is a preset method 
+  } else { // mistakes set as false in line 49
+    alert("Oh no! Too many mistakes! you can play again");
+  }
+  setDefaultValues(true); // resets all tiles
+  updateMistakes();
+  const footer = document.createElement("div"); // creates footer to house play again?
+  footer.classList.add("footer");
+  const button = document.createElement("button"); // creates button to relaunch game
+  button.textContent = "Play again";
+  button.onclick = handlePlayAgain; // launches game restart function on click
+  footer.appendChild(button);
+  body.appendChild(footer);
+  // make remaining tiles unclickable as game over due to too many mistakes
+  const tiles = document.querySelectorAll(".tile");
+  tiles.forEach((tile) => (tile.onclick = null));
 }
 
 function updateMistakes() {
-    
-    const score = document.querySelector('.score')
-    score.textContent = 'Remaining mistakes: ' + mistakes;
+  const score = document.querySelector(".score"); // class from div HTML line 25
+  score.textContent = "Remaining mistakes: " + mistakes; // either refreshes mistakes count with updated amount after line 46 or resets to default in case of lost()
 }
 
 function handlePlayAgain() {
-    const footer = document.querySelector('.footer')
-    footer.remove()
-    initialize()
+  const footer = document.querySelector(".footer");  // local access to footer element created in lost function line 116
+  footer.remove();
+  initialize(); // restarts whole game
 }
